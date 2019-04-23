31e1ba2af240067a2bd550131fe23cdd3283d55d
wexl <wexl@163.com>
2019-04-23 18:17:50 +0800

    增加新的eslit校验规则111

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,8 +9,8 @@ module.exports = {
     node: true,
     es6: true,
   },
-  //extends: "ivweb",//['plugin:vue/recommended', 'eslint:recommended'],
-  "extends": ["eslint:recommended", "plugin:react/recommended", "ivweb"],
+  extends: "ivweb",//['plugin:vue/recommended', 'eslint:recommended'],
+  //"extends": ["eslint:recommended", "plugin:react/recommended", "ivweb"],
 
   // add your custom rules here
   //it is base on https://github.com/vuejs/eslint-config-vue
