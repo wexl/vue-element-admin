b419b434f803af078c614df0918e73a517870f04
wexl <wexl@163.com>
2019-04-24 17:57:19 +0800

    表单练习 0424

--- src/views/nested/menu2/menu2-2/index.vue
+++ src/views/nested/menu2/menu2-2/index.vue
@@ -1,5 +1,26 @@
-<template functional>
-  <div style="padding:30px;">
-    <el-alert :closable="false" title="menu 2-2" type="warning" />
-  </div>
+<template>
+ <el-form v-model="formInline" class="">
+    <el-form-item label="输入数据">
+      <el-input v-model="formInline.user" placeholder="请输入名称"></el-input>
+    </el-form-item>
+ </el-form>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      formInline: {
+        user: '',
+        region: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit() {
+      console.log('submit!');
+    }
+  },
+  watch: {}
+}
+</script>
