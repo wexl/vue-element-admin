4e7d3647c9fe998994c4268eaa8cdf84db56584d
wexl <wexl@163.com>
2019-04-25 09:59:07 +0800

    事件绑定及v-for练习

--- src/views/nested/menu2/menu2-2/index.vue
+++ src/views/nested/menu2/menu2-2/index.vue
@@ -1,8 +1,16 @@
 <template>
  <el-form v-model="formInline" class="">
     <el-form-item label="输入数据">
-      <el-input v-model="formInline.user" placeholder="请输入名称"></el-input>
+      <el-input v-model="formInline.user" placeholder="请输入名称" @keyup.enter.native="onSubmit"></el-input>
     </el-form-item>
+   <el-button type="text" @click="clearList">清空列表</el-button>
+   <el-form-item label="列表">
+     <ul class="">
+       <li v-for="item in formInline.friends">
+         {{item.name}}
+       </li>
+     </ul>
+   </el-form-item>
  </el-form>
 </template>
 
@@ -12,13 +20,21 @@ export default {
     return {
       formInline: {
         user: '',
-        region: ''
+        friends: [
+          {name : 'tom'},
+          {name : 'jone'}
+        ]
       }
     }
   },
   methods: {
     onSubmit() {
       console.log('submit!');
+      this.formInline.friends.push({name : this.formInline.user})
+      this.formInline.user=''
+    },
+    clearList(){
+      this.formInline.friends=[]
     }
   },
   watch: {}
